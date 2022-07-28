7b01f708578c5b5c0c2aaa7e20240bee675d39e8
Takayuki Miura <6015abc@gmail.com>
2022-07-29 00:37:00 +0900

    調整

--- index.php
+++ index.php
@@ -23,27 +23,23 @@ $pageDir = $pageData->getPageDir($_SERVER["PHP_SELF"]);
 
 <body>
   <main class="bg-gray-100 rounded-2xl h-screen overflow-hidden relative">
-    <div class="flex items-start justify-between">
+    <div class="flex items-start">
       <div class="h-screen hidden lg:block my-4 ml-4 shadow-lg relative w-80">
         <!-- sidebar -->
         <?php include(dirname(__FILE__, 1) . "/php/view/partial/sidebar.php"); ?>
       </div>
 
-      <div class="flex flex-col w-full pl-0 md:p-4 md:space-y-4">
-        <!-- header -->
-        <?php include(dirname(__FILE__, 1) . "/php/view/partial/header.php"); ?>
-
-        <div class="overflow-auto h-screen pb-24 pt-2 pr-2 pl-2 md:pt-0 md:pr-0 md:pl-0">
+      <div class="flex flex-col pl-0 my-4 ml-4" style="width: 1280px;">
+        <div class="overflow-x-scroll h-screen pb-24">
           <div class="flex flex-row flex-wrap">
 
-            <!-- main section -->
             <div class="w-1/2 pr-4">
               <div class="mb-4 mx-0">
                 <div class="shadow-lg rounded-2xl bg-white p-4 hover:opacity-75 transition ease-in duration-200 mb-4">
                   <a href="/image-creator/market/" class="flex-row gap-2 flex justify-center items-center">
                     <div class="flex-shrink-0 w-2/12">
                       <span class="block relative">
-                        <img alt="" src="/image-creator/market/image/icon/market.svg" class="mx-auto object-contain h-24 w-24 " />
+                        <img alt="" src="/image-creator/market/image/icon/market.svg" class="mx-auto object-contain h-16 w-16 " />
                       </span>
                     </div>
                     <div class="flex flex-col w-8/12">
@@ -58,6 +54,7 @@ $pageDir = $pageData->getPageDir($_SERVER["PHP_SELF"]);
                 </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
