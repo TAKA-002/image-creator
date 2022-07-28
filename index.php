e748dbeee05e657a69c7aafd395296505b432eb2
Takayuki Miura <6015abc@gmail.com>
2022-07-29 01:13:38 +0900

    調整中

--- index.php
+++ index.php
@@ -22,18 +22,19 @@ $pageDir = $pageData->getPageDir($_SERVER["PHP_SELF"]);
 </head>
 
 <body>
-  <main class="bg-gray-100 rounded-2xl h-screen overflow-hidden relative">
-    <div class="flex items-start">
-      <div class="h-screen hidden lg:block my-4 ml-4 shadow-lg relative w-80">
+  <main class="bg-gray-100 rounded-2xl h-screen relative">
+    <div class="flex">
+      <div class="h-screen my-4 ml-4 shadow-lg relative" style="width: 320px;">
         <!-- sidebar -->
         <?php include(dirname(__FILE__, 1) . "/php/view/partial/sidebar.php"); ?>
       </div>
 
       <div class="flex flex-col pl-0 my-4 ml-4" style="width: 1280px;">
-        <div class="overflow-x-scroll h-screen pb-24">
-          <div class="flex flex-row flex-wrap">
+        <div class="h-screen pb-24">
+          <div class="flex flex-row flex-wrap justify-around">
 
-            <div class="w-1/2 pr-4">
+            <!-- マーケットコラム -->
+            <div style="width: 620px;">
               <div class="mb-4 mx-0">
                 <div class="shadow-lg rounded-2xl bg-white p-4 hover:opacity-75 transition ease-in duration-200 mb-4">
                   <a href="/image-creator/market/" class="flex-row gap-2 flex justify-center items-center">
@@ -47,7 +48,30 @@ $pageDir = $pageData->getPageDir($_SERVER["PHP_SELF"]);
                     </div>
                     <div class="flex-shrink-0 w-2/12">
                       <span class="block relative">
-                        <img alt="" src="/image-creator/common/images/icon/arrow.svg" class="mx-auto object-contain h-8 w-8 " />
+                        <img alt="" src="/image-creator/common/images/icon/arrow.svg" class="mx-auto object-contain h-4 w-4 " />
+                      </span>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <!-- 未定 -->
+            <div style="width: 620px;">
+              <div class="mb-4 mx-0">
+                <div class="shadow-lg rounded-2xl bg-white p-4 hover:opacity-75 transition ease-in duration-200 mb-4">
+                  <a href="" class="flex-row gap-2 flex justify-center items-center">
+                    <div class="flex-shrink-0 w-2/12">
+                      <span class="block relative">
+                        <img alt="" src="/image-creator/common/images/icon/nodecide.svg" class="mx-auto object-contain h-16 w-16 " />
+                      </span>
+                    </div>
+                    <div class="flex flex-col w-8/12">
+                      <span class="text-gray-600 text-lg font-medium">未定</span>
+                    </div>
+                    <div class="flex-shrink-0 w-2/12">
+                      <span class="block relative">
+                        <img alt="" src="/image-creator/common/images/icon/arrow.svg" class="mx-auto object-contain h-4 w-4 " />
                       </span>
                     </div>
                   </a>
--- php/view/partial/sidebar.php
+++ php/view/partial/sidebar.php
@@ -1,4 +1,4 @@
-<div class="bg-white h-full rounded-2xl">
+<div class="bg-white h-full rounded-2xl" style="width:320px;">
   <div class="flex items-center justify-center pt-6">
     <p class="font-bold text-xl">Image Creator</p>
   </div>
@@ -16,10 +16,7 @@
             <button type="submit" class="w-full font-thin uppercase text-gray-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500">
             <?php endif; ?>
             <span class="text-left">
-              <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1472 992v480q0 26-19 45t-45 19h-384v-384h-256v384h-384q-26 0-45-19t-19-45v-480q0-1 .5-3t.5-3l575-474 575 474q1 2 1 6zm223-69l-62 74q-8 9-21 11h-3q-13 0-21-7l-692-577-692 577q-12 8-24 7-13-2-21-11l-62-74q-8-10-7-23.5t11-21.5l719-599q32-26 76-26t76 26l244 204v-195q0-14 9-23t23-9h192q14 0 23 9t9 23v408l219 182q10 8 11 21.5t-7 23.5z">
-                </path>
-              </svg>
+              <img src="/image-creator/common/images/icon/home.svg" width="20px" height="20px" alt="">
             </span>
             <span class="mx-4 text-sm font-normal">HOME</span>
             </button>
@@ -36,10 +33,7 @@
             <button type="submit" class="w-full font-thin uppercase text-gray-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500">
             <?php endif; ?>
             <span class="text-left">
-              <svg width="20" height="20" fill="currentColor" class="m-auto" viewBox="0 0 2048 1792" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1024 1131q0-64-9-117.5t-29.5-103-60.5-78-97-28.5q-6 4-30 18t-37.5 21.5-35.5 17.5-43 14.5-42 4.5-42-4.5-43-14.5-35.5-17.5-37.5-21.5-30-18q-57 0-97 28.5t-60.5 78-29.5 103-9 117.5 37 106.5 91 42.5h512q54 0 91-42.5t37-106.5zm-157-520q0-94-66.5-160.5t-160.5-66.5-160.5 66.5-66.5 160.5 66.5 160.5 160.5 66.5 160.5-66.5 66.5-160.5zm925 509v-64q0-14-9-23t-23-9h-576q-14 0-23 9t-9 23v64q0 14 9 23t23 9h576q14 0 23-9t9-23zm0-260v-56q0-15-10.5-25.5t-25.5-10.5h-568q-15 0-25.5 10.5t-10.5 25.5v56q0 15 10.5 25.5t25.5 10.5h568q15 0 25.5-10.5t10.5-25.5zm0-252v-64q0-14-9-23t-23-9h-576q-14 0-23 9t-9 23v64q0 14 9 23t23 9h576q14 0 23-9t9-23zm256-320v1216q0 66-47 113t-113 47h-352v-96q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v96h-768v-96q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v96h-352q-66 0-113-47t-47-113v-1216q0-66 47-113t113-47h1728q66 0 113 47t47 113z">
-                </path>
-              </svg>
+              <img src="/image-creator/market/image/icon/market.svg" width="20px" height="20px" alt="">
             </span>
             <span class="mx-4 text-sm font-normal">マーケットコラム興味津々<br>注目予定</span>
             </button>
