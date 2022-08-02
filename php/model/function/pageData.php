e8d76e95d37f2345654a3eb6af41cdd56111d13a
Takayuki Miura <6015abc@gmail.com>
2022-08-03 01:56:44 +0900

    フォーマット。

--- php/model/function/pageData.php
+++ php/model/function/pageData.php
@@ -1,13 +1,14 @@
 <?php
 
-class pageData{
+class pageData
+{
 
   /**
    * 表示しているページのディレクトリ名を返す。
    */
-  public function getPageDir($selfPath){
-   $result = explode("/", $selfPath);
-   return $result[count($result) - 2];
+  public function getPageDir($selfPath)
+  {
+    $result = explode("/", $selfPath);
+    return $result[count($result) - 2];
   }
-
 }
