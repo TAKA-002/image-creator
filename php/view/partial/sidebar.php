4d33cf0de027e53406a2c8b2f8eea7920021776e
Takayuki Miura <6015abc@gmail.com>
2022-08-04 02:21:32 +0900

    サイドバーにテンプレートを追加

--- php/view/partial/sidebar.php
+++ php/view/partial/sidebar.php
@@ -26,6 +26,7 @@
       <!-- market start -->
       <form action="/image-creator/market/" method="POST">
         <input type="hidden" name="pos" value="market">
+        <input type="hidden" name="dispFlag" value="1">
 
         <?php if ($pageDir === "market") : ?>
           <button type="submit" class="w-full font-thin uppercase text-blue-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start bg-gradient-to-r from-white to-blue-100 border-r-4 border-blue-500 border-r-4 border-blue-500">
@@ -39,6 +40,26 @@
             </button>
       </form>
       <!-- market end -->
+
+      <!-- 追加時使用テンプレート start -->
+      <!-- actionに遷移先を記述 -->
+      <form action="" method="POST">
+        <input type="hidden" name="pos" value="market">
+        <input type="hidden" name="dispFlag" value="1">
+
+        <!-- ディレクトリ名を記述 -->
+        <?php if ($pageDir === "/") : ?>
+          <button type="submit" class="w-full font-thin uppercase text-blue-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start bg-gradient-to-r from-white to-blue-100 border-r-4 border-blue-500 border-r-4 border-blue-500">
+          <?php else : ?>
+            <button type="submit" class="w-full font-thin uppercase text-gray-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500">
+            <?php endif; ?>
+            <span class="text-left">
+              <img src="/image-creator/common/images/icon/nodecide.svg" width="20px" height="20px" alt="">
+            </span>
+            <span class="mx-4 text-sm font-normal">追加時使用テンプレート</span>
+            </button>
+      </form>
+      <!-- 追加時使用テンプレート end -->
 
     </div>
   </nav>
