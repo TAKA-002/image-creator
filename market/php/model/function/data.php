b3acceb37074a41739e12438fec1a5d2fc843fd7
Takayuki Miura <6015abc@gmail.com>
2022-08-14 06:07:49 +0900

    使用しないメソッドを削除

--- market/php/model/function/data.php
+++ market/php/model/function/data.php
@@ -204,13 +204,4 @@ class Data
       return $targetJsonData;
     }
   }
-
-
-  /**
-   * 現在使われているflags.cssファイルのパスを取得する（cssファイルを生成するときに使う）
-   */
-  private function getCssDataPath()
-  {
-    return dirname(__DIR__, 3) . "/css/flags.css";
-  }
 }
