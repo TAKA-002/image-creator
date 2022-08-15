486b19a886d75ae2078991c9e7a8eeb68da48790
Takayuki Miura <6015abc@gmail.com>
2022-08-15 21:26:33 +0900

    表示例をコメントで。

--- market/php/view/partial/imageTable.php
+++ market/php/view/partial/imageTable.php
@@ -7,6 +7,56 @@
       <div class="list__body">
         <ul id="js-list" class="list__body-wrap">
           <!-- jsで自動出力 -->
+          <!-- 
+          <li class="list__body-date"><time>5月2日(月)</time>
+            <ul class="plans">
+              <li class="icon-US">ISM製造業景況指数（4月）</li>
+              <li class="icon-NNF">製造業PMI（4月）</li>
+            </ul>
+          </li>
+          <li class="list__body-date"><time>5月3日(火)</time>
+            <ul class="plans">
+              <li class="icon-JP"><span style="color: #f00">祝日・憲法記念日（市場休場）</span></li>
+              <li class="icon-AUS">オーストラリア中銀政策金利発表</li>
+            </ul>
+          </li>
+          <li class="list__body-date"><time>5月4日(水)</time>
+            <ul class="plans">
+              <li class="icon-JP"><span style="color: #f00">祝日・みどりの日（市場休場）</span></li>
+              <li class="icon-US">貿易収支（3月）</li>
+              <li class="icon-NNF">ISM非製造業景況指数（4月）</li>
+            </ul>
+          </li>
+          <li class="list__body-date"><time>5月5日(木)</time>
+            <ul class="plans">
+              <li class="icon-JP"><span style="color: #f00">祝日・こどもの日（市場休場）</span></li>
+              <li class="icon-US">FOMC政策金利発表</li>
+              <li class="icon-NNF">パウエルFRB議長記者会見</li>
+              <li class="icon-BR">ブラジル中銀政策金利発表</li>
+            </ul>
+          </li>
+          <li class="list__body-date"><time>5月6日(金)</time>
+            <ul class="plans">
+              <li class="icon-JP">消費者物価指数（東京都区部）</li>
+              <li class="icon-US">雇用統計（4月）</li>
+            </ul>
+          </li>
+          <li class="list__body-date"><time>5月10日(火)</time>
+            <ul class="plans">
+              <li class="icon-JP">本決算：住友商事、伊藤忠商事、三菱商事</li>
+            </ul>
+          </li>
+          <li class="list__body-date"><time>5月11日(水)</time>
+            <ul class="plans">
+              <li class="icon-JP">本決算：トヨタ自動車、ソフトバンク</li>
+            </ul>
+          </li>
+          <li class="list__body-date"><time>5月13日(金)</time>
+            <ul class="plans">
+              <li class="icon-JP">本決算：みずほFG、三井住友FG</li>
+            </ul>
+          </li>
+          -->
         </ul>
       </div>
     </div>
